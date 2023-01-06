b6604bbb9181e47820b67946e0813d7211189366
j4vlon <fedot.pushkin98@gmail.com>
2023-01-06 18:10:44 +0500

    add media queries

--- js/script.js
+++ js/script.js
@@ -1,44 +1,54 @@
-$(function() {
-    //do something
-      
-      $(".btn-round").click({animateIn: "closeButton", animateOut: "plusButton"}, animate_function);
-      
-      function animate_function(event){
-          if( $(this).hasClass(event.data.animateIn) ) {
-                $(this).removeClass(event.data.animateIn).addClass(event.data.animateOut);
-               }
-            else if( $(this).hasClass(event.data.animateOut) ) {
-               $(this).removeClass(event.data.animateOut).addClass(event.data.animateIn);
-            }
-            else {
-              $(this).addClass('animated ' + event.data.animateIn);
-            }     
-      }
-      
-  //end do something     
-	});
+$(function () {
+  //do something
+
+  $(".btn-round").click(
+    { animateIn: "closeButton", animateOut: "plusButton" },
+    animate_function
+  );
+
+  function animate_function(event) {
+    if ($(this).hasClass(event.data.animateIn)) {
+      $(this).removeClass(event.data.animateIn).addClass(event.data.animateOut);
+    } else if ($(this).hasClass(event.data.animateOut)) {
+      $(this).removeClass(event.data.animateOut).addClass(event.data.animateIn);
+    } else {
+      $(this).addClass("animated " + event.data.animateIn);
+    }
+  }
+
+  //end do something
+});
 
 // slider js
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:24,
-  width:3300,
-  navText: ["<span aria-label='Previous'><i class='fa-sharp fa-solid fa-arrow-left'></i></span>", "<span aria-label='Next'><i class='fa-sharp fa-solid fa-arrow-right'></i></span>"],
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 24,
+  width: 3300,
+  navText: [
+    "<span aria-label='Previous'><i class='fa-sharp fa-solid fa-arrow-left'></i></span>",
+    "<span aria-label='Next'><i class='fa-sharp fa-solid fa-arrow-right'></i></span>",
+  ],
   dots: false,
-  responsiveClass:true,
-  responsive:{
-      0:{
-          items:1,
-          nav:true
-      },
-      600:{
-          items:3,
-          nav:false
-      },
-      1000:{
-          items:4,
-          nav:true,
-          loop:false
-      }
-  }
-})
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+    },
+    600: {
+      items: 3,
+      nav: false,
+    },
+    1240: {
+      items: 3,
+      nav: true,
+      loop: false,
+      margin: 15,
+    },
+    1400: {
+      items: 4,
+      nav: true,
+      loop: false,
+    },
+  },
+});
